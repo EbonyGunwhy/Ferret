be64328f6c35e3872833fe7161ecd54601d506f3
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2022-06-21 17:51:51 +0100

    Update SimpleModels.py

--- Developer/ModelLibrary/SimpleModels.py
+++ Developer/ModelLibrary/SimpleModels.py
@@ -2,12 +2,50 @@ import numpy as np
 from SupportModules.Model import Model, ModelParameter, ModelConstant, ModelVariable 
 from SupportModules.GraphSupport import LineColours
 
-def linear(inputData, a, b, constantsString=None):
+#*********************************************************************************
+#** Models
+#*********************************************************************************
+def quadraticModel(inputData, a, b, constantsString):
+    #y = ax^2 + bx + c
+    constantsDict = eval(constantsString) 
+    c = float(constantsDict['c'])
+    return np.multiply((inputData*inputData),a) + np.multiply(inputData, b) + c
+
+
+def straightLineModel(inputData, m, constantsString):
+    constantsDict = eval(constantsString) 
+    c = float(constantsDict['c'])
+    return np.multiply(inputData,m) + c
+
+
+def linearModel(inputData, a, b, constantsString=None):
     return np.multiply(inputData,a) + b
 
 #*******************************************************************************
 #** Define parameters for the model
 #*******************************************************************************
+def setUpParameterForStraightLineModel():
+    """
+    This optional function returns a list of parameter objects.
+
+    An object is created for each parameter using the ModelParameter class 
+    and added to the list, that is returned by this function.
+
+    In the returnModelList function, this function is used to 
+    populate the parameterList argument. 
+    """
+    paramList = []
+    m = ModelParameter(shortName='m',
+                        longName='m',
+                        units='s-1', 
+                        defaultValue=1.0, 
+                        stepSize=1, 
+                        precision=1, 
+                        minValue=1, 
+                        maxValue=100.0)
+    paramList.append(m)    
+    return paramList
+
 def setUpParametersForLinearModel():
     """
     This optional function returns a list of parameter objects.
@@ -39,10 +77,60 @@ def setUpParametersForLinearModel():
     paramList.append(b)    
     return paramList
 
+def setUpParametersForQuadraticModel():
+    """
+    This optional function returns a list of parameter objects.
+
+    An object is created for each parameter using the ModelParameter class 
+    and added to the list, that is returned by this function.
+
+    In the returnModelList function, this function is used to 
+    populate the parameterList argument. 
+    """
+    paramList = []
+    a = ModelParameter(shortName='a',
+                        longName='a',
+                        units='mL/min/mL', 
+                        defaultValue=4.0, 
+                        stepSize=1, 
+                        precision=1, 
+                        minValue=1, 
+                        maxValue=100.0)
+    paramList.append(a)    
+    b = ModelParameter(shortName='b',
+                        longName='b',
+                        units='mL/min/mL', 
+                        defaultValue=2, 
+                        stepSize=1, 
+                        precision=1, 
+                        minValue=1, 
+                        maxValue=100.0)
+    paramList.append(b)    
+    return paramList
+
+#*******************************************************************************
+#** Define constants for  models
+#*******************************************************************************
+def setUpConstantForYAxisIntersection():
+    """
+    This optional function returns a list of constant objects.
+
+    An object is created for each constant using the ModelConstant class 
+    and added to the list, that is returned by this function.
+
+    In the returnModelList function, this function is used to 
+    populate the constantList argument. 
+    """
+    constantList = []
+    c = ModelConstant(shortName='c', longName='Y Axis Intersection', defaultValue=1.0, stepSize=10.0,
+                       precision=1, units = None, minValue=0, maxValue=10000, listValues=[])
+    constantList.append(c)
+    return constantList
+
 #*******************************************************************************
 # Define variables for both models
 #*******************************************************************************
-def setUpVariablesForLinearModel():
+def setUpVariablesForAllModels():
     """
     This optional function returns a list of variable objects.
 
@@ -70,15 +158,31 @@ def returnModelList():
     An object is created for each mathematic model using the Model class 
     and added to the list, that is returned by this function.
     """
-    Linear = Model(shortName='Linear', 
+    linear = Model(shortName='Linear', 
                      longName ='Linear', 
                      xDataInputOnly = True,
-                     modelFunction = linear,
+                     modelFunction = linearModel,
                      parameterList = setUpParametersForLinearModel(), 
-                     variablesList = setUpVariablesForLinearModel()
+                     variablesList = setUpVariablesForAllModels()
                      )
+
+    straightLine = Model(shortName='Straight Line',
+                         longName='Straight Line',
+                         xDataInputOnly = True,
+                         modelFunction = straightLineModel,
+                         parameterList = setUpParameterForStraightLineModel(), 
+                         variablesList = setUpVariablesForAllModels(),
+                         constantsList = setUpConstantForYAxisIntersection())
+
+    quadratic = Model(shortName='Quadratic',
+                         longName='Quadratic',
+                         xDataInputOnly = True,
+                         modelFunction = quadraticModel,
+                         parameterList = setUpParametersForQuadraticModel(), 
+                         variablesList = setUpVariablesForAllModels(),
+                         constantsList = setUpConstantForYAxisIntersection())
    
-    return[Linear]
+    return[linear, straightLine, quadratic]
 
 
 def returnDataFileFolder():
